def7d44fa876949064efd9d30a5df0ab453aa108
sagar-kumar-yadav <sk5323912@gmail.com>
2024-10-07 20:59:46 +0530

    new c

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/Auth.jsx";
-import { SearchProvider } from "./context/Search.jsx";
 import { CartProvider } from "./context/cart.jsx";
+import { AuthProvider } from "./context/auth.jsx";
+import { SearchProvider } from "./context/search.jsx";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
