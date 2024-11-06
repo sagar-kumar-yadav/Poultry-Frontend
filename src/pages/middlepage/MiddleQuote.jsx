ef979f1c0d37b37182de3317c9b801fa1b656294
sagar-kumar-yadav <sk5323912@gmail.com>
2024-11-06 22:32:40 +0530

    nnnf

--- src/pages/middlepage/MiddleQuote.jsx
+++ src/pages/middlepage/MiddleQuote.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MiddleQuote = () => {
   return (
-    <div className=" uppercase w-full mx-auto flex items-center justify-center relative top-0 flex-col max-sm:top-[-8rem]">
+    <div className=" uppercase w-full mx-auto flex items-center justify-center relative flex-col max-sm:top-[-8rem]">
       <div className="w-[60%] text-5xl font-medium text-center max-sm:text-2xl max-sm:w-[90%]">
         We create limitless styles by expanding our product range, from
         international products to convert local products
